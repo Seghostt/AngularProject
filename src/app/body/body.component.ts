ddef42c0d26e136d8e0d49ace9f8309303b9307a
seghostt <deimos@outlook.es>
2022-10-21 16:59:57 -0300

    project

---
+++ src/app/body/body.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { EventType } from '@angular/router';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+alert("xd")
+
