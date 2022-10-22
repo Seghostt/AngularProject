1cc5c50e5263c3aebfd6ba67f26882637e5dc8ab
seghostt <deimos@outlook.es>
2022-10-22 07:24:33 -0300

    second commit

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -14,5 +14,65 @@ export class BodyComponent implements OnInit {
   }
 
 }
-alert("xd")
+type QeueItem = () => Promise<void>
+var IconsText: HTMLElement= document.getElementById('Main')!
 
+class typewriter{
+queue: QeueItem[] = []   
+element: HTMLElement
+loop: boolean
+typingspeed: number
+deletingspeed: number
+
+constructor(IconsText: HTMLElement,{loop = false, typingspeed = 50,deletingspeed = 50 } = {}){
+this.element = document.createElement("div")
+IconsText.append(this.element)
+this.loop = loop
+this.typingspeed = typingspeed
+this.deletingspeed = deletingspeed
+}
+
+typestring(string: string){
+  this.queue.push(() => {
+    return new Promise((resolve,reject) =>{
+      let i = 0;
+      const interval = setInterval(() => {
+        this.element.append(string[i])
+        i++
+        if(i >= string.length){
+          clearInterval(interval)
+          resolve()
+        }
+      }, this.typingspeed)
+    })
+  
+  
+  })
+
+}
+
+deletestring(number: number){
+  console.log(number)
+  return this
+}
+
+deleteall(deletespeed = this.deletingspeed){
+  console.log(deletespeed)
+}
+
+pauseFor(duration: number){
+  console.log(duration)
+}
+
+async start (){
+  for(let i of this.queue){
+    await i()
+  }
+  return this
+}
+}
+
+const typewriter1 = new typewriter(document.body, {loop: true})
+
+typewriter1.typestring("Linkedin")
+typewriter1.start()
